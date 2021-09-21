1a04544ce104fee9404a6b3a6e65aa881b78e9af
Herobone <theherobone@gmail.com>
2021-09-21 14:45:49 +0200

    Add TicTacToe

---
+++ src/helpers/TicTacToeUtils.ts
@@ -0,0 +1,50 @@
+/*****************************
+ * Sober Sailor - The online Party Game
+ * Copyright (c) 2021.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+import { TicOptions } from "../models/TicTacToe";
+
+export class TicTacToeUtils {
+  static calculateWinner(squares: TicOptions[]): TicOptions | "tie" {
+    const lines = [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+      [0, 4, 8],
+      [2, 4, 6],
+    ];
+    let winner: TicOptions | "tie" = null;
+    lines.forEach((line: number[]) => {
+      const [a, b, c] = line;
+      if (
+        squares[a] &&
+        squares[a] === squares[b] &&
+        squares[a] === squares[c]
+      ) {
+        winner = squares[a];
+      }
+    });
+    if (winner === null && !squares.includes(null)) {
+      // every square is full
+      winner = "tie";
+    }
+    return winner;
+  }
+}
