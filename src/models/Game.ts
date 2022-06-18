58e71f669b9ea9f5a6f269d72834e43e9ed72713
Julius G <theherobone@gmail.com>
2022-06-18 13:50:45 +0200

    🧵 Change currentTask to string to support TOD

--- src/models/Game.ts
+++ src/models/Game.ts
@@ -22,27 +22,9 @@ import {
   ExternalEvaluationScoreboard,
 } from "./EvaluationScoreboard";
 
-/*****************************
- * Sober Sailor - The online Party Game
- * Copyright (c) 2021.
- *
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program.  If not, see <https://www.gnu.org/licenses/>.
- */
-
 export interface IGame {
   gameID: string;
-  currentTask: number | null;
+  currentTask: string | null;
   type: string | null;
   taskTarget: string | null;
   penalty: number;
@@ -58,7 +40,7 @@ export interface IGame {
 }
 
 export interface IGameExternal<TimeStampClass> {
-  currentTask: number | null;
+  currentTask: string | null;
   type: string | null;
   taskTarget: string | null;
   penalty: number;
@@ -77,7 +59,7 @@ export interface IGameExternal<TimeStampClass> {
 export class Game implements IGame {
   constructor(
     readonly gameID: string,
-    readonly currentTask: number | null,
+    readonly currentTask: string | null,
     readonly type: string | null,
     readonly taskTarget: string | null,
     readonly penalty: number,
